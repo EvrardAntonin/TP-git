8d5c1dc5605b90b3183f309b42ea8e054f645fd8
Antonin Evrard <a.evrard@iia-laval.fr>
2019-01-31 14:14:38 +0100

    add partner name

--- src/application.bash
+++ src/application.bash
@@ -1,4 +1,4 @@
 ### Application.bash - 201x - Antonin EVRARD ###
-echo "version 1.1"
-echo "Copyright Company - 201x"
+echo "version 1.2"
+echo "Copyright Company - 201x Institut Informatique Appliqué - Laval"
 echo "Build 10:00"
